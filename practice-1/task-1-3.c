d051155051510d191abcf4cde6efa9d9a2b4c3e4
4drenal1n <lg.brutalelite@mail.ru>
2026-03-27 17:03:18 +0300

    Update task-1-3.c

--- practice-1/task-1-3.c
+++ practice-1/task-1-3.c
@@ -3,14 +3,14 @@
 int main() {
     int a, b, product;
 
-    // Запрос двух целых чисел
+   
     printf("Enter two integers: ");
     scanf("%d %d", &a, &b);
 
-    // Вычисление произведения
+   
     product = a * b;
 
-    // Вывод результата в требуемом формате
+  
     printf("Product: %d * %d = %d\n", a, b, product);
 
     return 0;
